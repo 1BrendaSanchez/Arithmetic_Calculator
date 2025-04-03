b46406acc19af48da8376ad77fdb955b06d4ac96
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2025-04-02 18:40:03 -0700

    added subtraction multiplication and division to arithmetic calculator

--- main.py
+++ main.py
@@ -15,20 +15,42 @@ num2.grid(column=0, row=1)
 num2Entry = tk.Entry()
 num2Entry.grid(column=1, row=1)
 
+opLabel = tk.Label(text="Select the Operation: ")
+opLabel.grid(column=0, row=2)
+
+myOption = tk.StringVar(window)
+myOption.set("Select")
+opMenu = tk.OptionMenu(window, myOption, "Add", "Subtract", "Multiply", "Divide")
+opMenu.grid(column=1, row=2)
+
 
 # button event method
 
-def addNumbers():
+def mathOperations():
     a = int(num1Entry.get())
     b = int(num2Entry.get())
-    s = a + b
-    result = "The sum is {0}".format(s)
-    textArea = tk.Text(master=window, height=7, width=15)
+    op = myOption.get()
+    if (op == 'Add'):
+        s = a + b
+        result = "The result is {0}".format(s)
+    elif (op == 'Subtract'):
+        s = a - b
+        result = "The result is {0}".format(s)
+    elif (op == 'Multiply'):
+        s = a * b
+        result = "The result is {0}".format(s)
+    elif (op == 'Divide'):
+        s = a / b
+        result = "The result is {0}".format(s)
+    else:
+        result = "Please select an operation"
+    textArea = tk.Text(master=window, height=7, width=20)
     textArea.grid(column=1, row=3)
     textArea.insert(tk.END, result)
 
 
-button = tk.Button(window, text="Calculate Sum", command=addNumbers, bg="lightgreen")
-button.grid(column=1, row=2)
+button = tk.Button(window, text="Calculate", command=mathOperations, bg="lightgreen")
+button.grid(column=0, row=3)
 
 window.mainloop()
+
