89d0968865253aa6eeac1ff357c8f790c9de88d3
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2025-04-02 18:20:59 -0700

    addition is working

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ window = tk.Tk()
 window.geometry("600x300")
 window.title("Calculator App")
 
+# creating the widgets
 num1 = tk.Label(text="Enter 1st Number: ")
 num1.grid(column=0, row=0)
 num1Entry = tk.Entry()
@@ -14,7 +15,20 @@ num2.grid(column=0, row=1)
 num2Entry = tk.Entry()
 num2Entry.grid(column=1, row=1)
 
-button = tk.Button(window, text="Calculate Sum")
+
+# button event method
+
+def addNumbers():
+    a = int(num1Entry.get())
+    b = int(num2Entry.get())
+    s = a + b
+    result = "The sum is {0}".format(s)
+    textArea = tk.Text(master=window, height=7, width=15)
+    textArea.grid(column=1, row=3)
+    textArea.insert(tk.END, result)
+
+
+button = tk.Button(window, text="Calculate Sum", command=addNumbers, bg="lightgreen")
 button.grid(column=1, row=2)
 
 window.mainloop()
